d41bfb044713ef134ef4493aba1f261714172d89
Oliver Kopp <kopp.dev@gmail.com>
2016-03-30 22:05:58 +0200

    Fix broken scrolling

--- site.js
+++ site.js
@@ -72,7 +72,7 @@ $.extend($.easing,
 
 $(document).ready(function (){
 
-    $('nav li a').navScroller();
+    $('nav li a:not(.absolute)').navScroller();
 
     //section divider icon click gently scrolls to reveal the section
 	$(".sectiondivider").on('click', function(event) {
