09fb754f23c7d274336207182785c6b03b6f6b81
Tobias Diez <tobiasdiez@gmx.de>
2020-05-17 20:05:04 +0200

    Improve header

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,40 +1,8 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function () {
-  $('a.page-scroll').bind('click', function (event) {
-    var $anchor = $(this)
-    $('html, body')
-      .stop()
-      .animate(
-        {
-          scrollTop: $($anchor.attr('href')).offset().top,
-        },
-        1500,
-        'easeInOutExpo'
-      )
-    event.preventDefault()
-  })
-})
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-  target: '.navbar-fixed-top',
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function () {
-  $('.navbar-toggle:visible').click()
-})
-
-$('div.modal').on('show.bs.modal', function () {
-  var modal = this
-  var hash = modal.id
-  window.location.hash = hash
-  window.onhashchange = function () {
-    if (!location.hash) {
-      $(modal).modal('hide')
-    }
-  }
-})
+// Closes the responsive menu on menu item click
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
 
 // OS specific download
 if (window.navigator.userAgent.indexOf('Win') != -1) {
