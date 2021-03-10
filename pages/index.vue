bcd2cdfbf6de26c90fe612ea4e0ca61a3ca429f9
Tobias Diez <code@tobiasdiez.com>
2021-03-10 19:38:07 +0100

    Use tailwind css

--- pages/index.vue
+++ pages/index.vue
@@ -38,18 +38,18 @@
   </div>
 </template>
 
-<script>
-export default {
-  async asyncData ({ $http }) {
-    const test = await $http.$get('/api/test')
-    return {
-      test
-    }
-  }
-}
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
 </script>
 
-<style scoped>
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -60,16 +60,8 @@ export default {
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
