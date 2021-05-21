b674f571941643b392af3d267698940f230e4696
mohit <mohitjangra038@gmail.com>
2021-05-21 13:05:21 +0530

    add-mutation

--- api/tsyringe.config.ts
+++ api/tsyringe.config.ts
@@ -1,8 +1,13 @@
 import 'reflect-metadata'
 import { PrismaClient } from '@prisma/client'
+import Redis from 'ioredis'
 
 import { container, instanceCachingFactory } from 'tsyringe'
 
 container.register<PrismaClient>(PrismaClient, {
   useFactory: instanceCachingFactory<PrismaClient>(() => new PrismaClient()),
+})
+
+container.register<Redis.Redis>('Redis', {
+  useFactory: (_) => new Redis(),
 })
