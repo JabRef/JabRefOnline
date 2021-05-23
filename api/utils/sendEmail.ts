eeea098c5ce48d7e3cd59105ab62aa217c13d3d8
mohit <mohitjangra038@gmail.com>
2021-05-24 01:48:37 +0530

    add comments

--- api/utils/sendEmail.ts
+++ api/utils/sendEmail.ts
@@ -1,10 +1,10 @@
 import nodemailer from 'nodemailer'
 
-// async..await is not allowed in global scope, must use a wrapper
 export async function sendEmail(to: string, html: string) {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
   const testAccount = await nodemailer.createTestAccount()
+  // eslint-disable-next-line no-console
   console.log(testAccount)
 
   // create reusable transporter object using the default SMTP transport
@@ -24,11 +24,9 @@ export async function sendEmail(to: string, html: string) {
     subject: 'Change Password', // Subject line
     html, // plain text body
   })
-
+  // eslint-disable-next-line no-console
   console.log('Message sent: %s', info.messageId)
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
   // Preview only available when sending through an Ethereal account
+  // eslint-disable-next-line no-console
   console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
