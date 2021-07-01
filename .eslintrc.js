0f0fc24c430a48219f4c104361ac401dae7726fe
Tobias Diez <code@tobiasdiez.com>
2021-07-01 17:38:08 +0200

    Add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,14 +45,58 @@ module.exports = {
     {
       files: ['*.graphql'],
       parser: '@graphql-eslint/eslint-plugin',
+      parserOptions: {
+        operations: ['./middleware/**/*.graphql'],
+      },
       plugins: ['@graphql-eslint'],
       rules: {
-        '@graphql-eslint/known-directives': 'error',
+        // Workaround for for bug in prettier, can be removed after https://github.com/prettier/eslint-plugin-prettier/pull/413
+        'prettier/prettier': [
+          2,
+          {
+            parser: 'graphql',
+          },
+        ],
+        // Make sure to not prefix id names with typename, i.e. 'id' instead of 'userId'.
+        '@graphql-eslint/avoid-typename-prefix': 'error',
+        // Requires all types to be reachable at some level by root level fields.
+        '@graphql-eslint/no-unreachable-types': 'error',
+        // Enforces that deprecated fields or enum values are not in use by operations.
+        // TODO: Set this to error once we follow this convention
+        '@graphql-eslint/no-deprecated': 'warn',
+        // Enforces unique fragment name.
+        '@graphql-eslint/unique-fragment-name': 'error',
+        // Enforces unique operation names.
+        // TODO: Does not work yet
+        // '@graphql-eslint/unique-operation-name': 'error',
+        // Requires to use """ or " for adding a GraphQL description instead of #.
+        // TODO: Does not work yet, so set to warn
+        '@graphql-eslint/no-hashtag-description': 'warn',
+        // Requires sname for your GraphQL operations.
+        '@graphql-eslint/no-anonymous-operations': 'error',
+        // Make sure to not add the operation type to the name of the operation, e.g. 'user' instead of 'userQuery'.
+        '@graphql-eslint/no-operation-name-suffix': 'error',
+        // Requires all deprecation directives to specify a reason
+        '@graphql-eslint/require-deprecation-reason': ['error'],
+        // Enforces descriptions in your type definitions
+        '@graphql-eslint/require-description': [
+          'warn',
+          { on: ['ObjectTypeDefinition', 'FieldDefinition'] },
+        ],
+        // Checks for duplicate fields in selection set, variables in operation definition, or in arguments set of a field.
+        '@graphql-eslint/avoid-duplicate-fields': ['error'],
+        // Requires mutation argument to be always called "input" and input type to be called Mutation name + "Input".
+        // TODO: Set this to error once we follow this convention
+        '@graphql-eslint/input-name': ['warn', { checkInputType: true }],
       },
     },
     {
       files: ['*.tsx', '*.ts', '*.jsx', '*.js'],
       processor: '@graphql-eslint/graphql',
+      rules: {
+        // Workaround for for bug in prettier, can be removed after https://github.com/prettier/eslint-plugin-prettier/pull/415
+        'prettier/prettier': 0,
+      },
     },
   ],
 }
