8e6e51f2022432a3f9e884928eab62ea948a794d
Tobias Diez <tobiasdiez@gmx.de>
2021-08-31 13:10:28 +0200

    Gracefully shutdown http server if apollo terminates

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,14 @@ import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import './tsyringe.config'
 import { container } from 'tsyringe'
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { ApolloServerPluginDrainHttpServer, ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { buildContext } from './context'
 import { loadSchema } from './schema'
 import PassportInitializer from './user/passport-initializer'
 
 // Create express instance
 const app = express()
+const httpServer = http.createServer(app);
 
 const passportInitializer = container.resolve(PassportInitializer)
 passportInitializer.initialize()
@@ -21,6 +22,8 @@ const server = new ApolloServer({
   plugins: [
     // Enable Apollo Studio in development, and also in production (at least for now)
     ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    // Gracefully shutdown HTTP server when Apollo server terminates 
+    ApolloServerPluginDrainHttpServer({ httpServer }),
   ],
 })
 
