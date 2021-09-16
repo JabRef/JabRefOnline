5ac808cae53e78b9ea07656d65040a21abc199a2
Tobias Diez <code@tobiasdiez.com>
2021-09-16 19:05:02 +0200

    Enable Vite for development

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,7 @@
   <div class="container"></div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({})
+</script>
