288512bdff8f2841444ca6d7cae71dececd8d4cc
Tobias Diez <tobiasdiez@gmx.de>
2021-10-04 23:21:58 +0200

    Update resolvers.ts

--- api/user/resolvers.ts
+++ api/user/resolvers.ts
@@ -78,7 +78,7 @@ export class Mutation {
         problems: [
           {
             path: 'Email or Password',
-            message: ('message' in info ? info?.message : info) || 'Unknown error while logging in.',
+            message: (typeof info === 'string' ? info : info?.message) || 'Unknown error while logging in.',
           },
         ],
       }
