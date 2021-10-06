00d686175daafce9bcf8d9d731e40f03f2ea29be
Mohit <62339705+mohit038@users.noreply.github.com>
2021-10-06 19:10:22 +0530

    update query on result

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -11,28 +11,20 @@
 </template>
 
 <script lang="ts">
-import { useResult, useQuery } from '@vue/apollo-composable'
-import {
-  ref,
-  defineComponent,
-  onMounted,
-  onUnmounted,
-} from '@vue/composition-api'
+import { useQuery } from '@vue/apollo-composable'
+import { defineComponent, onMounted, onUnmounted } from '@vue/composition-api'
 import { gql } from '~/apollo'
 import { useUiStore } from '~/store'
 
-const FIRST = 5
+const FIRST = 3
 
 export default defineComponent({
   middleware: ['authenticated'],
 
   setup() {
     const ui = useUiStore()
-    const cursor = ref<string>('')
-    // eslint-disable-next-line @typescript-eslint/no-array-constructor
-    const documents = ref(Array())
 
-    const { result, refetch, onResult } = useQuery(
+    const { result, fetchMore } = useQuery(
       gql(/* GraphQL */ `
         query GetDocuments(
           $groupId: ID
@@ -63,64 +55,72 @@ export default defineComponent({
         groupId: ui.selectedGroupId,
         query: ui.activeSearchQuery,
         first: FIRST,
-        cursor: cursor.value,
+        cursor: '',
       })
     )
 
-    onResult((data) => {
-      const documentResult = data.data.me?.documents.edges.map(
-        (edge) => edge.node
-      )
-      if (documentResult !== undefined) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        documents.value = [...documents.value, ...documentResult]
-      }
-    })
-    const newCursor = useResult(
-      result,
-      null,
-      (data) => data?.me?.documents.pageInfo.nextCursor
-    )
-    cursor.value = newCursor.value ? newCursor.value : ''
-
-    const handleScroll = async () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        await loadMoreDocuments()
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        result.value?.me?.documents.pageInfo.nextCursor !== ''
+      ) {
+        loadMoreDocuments()
       }
     }
 
     onMounted(() => {
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
       window.addEventListener('scroll', handleScroll)
     })
 
     onUnmounted(() => {
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
       window.removeEventListener('scroll', handleScroll)
     })
 
-    const loadMoreDocuments = async () => {
-      const documentResult = await refetch({
-        groupId: ui.selectedGroupId,
-        query: ui.activeSearchQuery,
-        cursor: cursor.value,
-        first: FIRST,
+    const loadMoreDocuments = () => {
+      void fetchMore({
+        query: gql(/* GraphQL */ `
+          query GetDocuments(
+            $groupId: ID
+            $query: String
+            $first: Int
+            $cursor: String
+          ) {
+            me {
+              id
+              documents(
+                filterBy: { groupId: $groupId, query: $query }
+                first: $first
+                cursor: $cursor
+              ) {
+                edges {
+                  node {
+                    ...DocumentForView
+                  }
+                }
+                pageInfo {
+                  nextCursor
+                }
+              }
+            }
+          }
+        `),
+        variables: {
+          cursor: result.value?.me?.documents.pageInfo.nextCursor,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          return {
+            ...previousResult,
+            me: {
+              ...previousResult.me,
+              documents: fetchMoreResult?.me?.documents,
+            },
+          }
+        },
       })
-
-      cursor.value =
-        documentResult?.data.me?.documents.pageInfo.nextCursor || ''
-
-      const newDocuments = documentResult?.data.me?.documents.edges
-        ?.filter((edge) => edge?.node)
-        .map((edge) => edge?.node)
-
-      if (newDocuments) {
-        documents.value.push(...newDocuments)
-      }
     }
 
     return {
-      documents,
+      result: document,
     }
   },
 })
