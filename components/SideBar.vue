fe7d158f950083653ba1b2cf228e815ce6e571f8
Tobias Diez <code@tobiasdiez.com>
2021-11-07 19:03:28 +0100

    Migrate to other tree library

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -83,57 +83,63 @@
         Groups
       </p>
 
-      <vue-tree-list
-        v-if="groupsTree"
+      <BaseTree
+        v-if="groups"
+        :tree-data="groups"
+        children-key="children"
+        :gap="7"
         class="groupsTree"
-        :model="groupsTree"
-        :default-expanded="true"
-        @click="onGroupClicked"
       >
-        <template #leafNameDisplay="slotProps">
+        <template #default="{ node, tree }">
           <span
-            class="
-              capitalize
-              font-medium
-              text-sm
-              hover:text-teal-600
-              transition
-              ease-in-out
-              duration-500
-            "
+            v-if="node.$children.length > 0"
+            class="-ml-8 mr-4 w-4 text-gray-400 inline-block"
+            @click="tree.toggleFold(node)"
           >
-            {{ slotProps.model.name }}
+            <FontAwesomeIcon
+              v-if="node.$folded"
+              icon="angle-down"
+              size="sm"
+              fixed-width
+            />
+            <FontAwesomeIcon
+              v-else
+              icon="angle-up"
+              size="sm"
+              class=""
+              fixed-width
+            />
           </span>
+          <button
+            class="hover:bg-gray-200 pl-1 pr-1"
+            @click="onGroupClicked(node)"
+          >
+            <FontAwesomeIcon
+              :icon="node.icon"
+              size="sm"
+              class="mr-2 pb-0.5"
+              fixed-width
+            />
+            <span class="capitalize font-medium text-sm hover:text-teal-600">
+              {{ node.name }}
+            </span>
+          </button>
         </template>
-        <span slot="addTreeNodeIcon" class="icon"></span>
-        <span slot="addLeafNodeIcon" class="icon"></span>
-        <span slot="editNodeIcon" class="icon"></span>
-        <span slot="delNodeIcon" class="icon"></span>
-        <span slot="leafNodeIcon" class="icon"></span>
-        <template #treeNodeIcon="slotProps">
-          <FontAwesomeIcon
-            :icon="slotProps.model.icon"
-            size="sm"
-            class="mr-2 pb-0.5"
-            fixed-width
-          />
-        </template>
-      </vue-tree-list>
+      </BaseTree>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { computed, defineComponent } from '@nuxtjs/composition-api'
-// @ts-ignore: No type infos available
-import { VueTreeList, Tree } from 'vue-tree-list'
+import { defineComponent } from '@nuxtjs/composition-api'
 import { useResult, useQuery } from '@vue/apollo-composable'
+import { BaseTree } from '@he-tree/vue2'
 import { gql } from '~/apollo'
 import { useUiStore } from '~/store'
+import '@he-tree/vue2/dist/he-tree-vue2.css'
 
 export default defineComponent({
   components: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    VueTreeList,
+    BaseTree,
   },
   setup() {
     const { result } = useQuery(
@@ -156,10 +162,6 @@ export default defineComponent({
       `)
     )
     const groups = useResult(result, null, (data) => data?.me?.groups)
-    const groupsTree = computed(() =>
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-      groups.value != null ? new Tree(groups.value) : null
-    )
 
     const uiStore = useUiStore()
     function onGroupClicked(group: { id: string }) {
@@ -167,7 +169,7 @@ export default defineComponent({
     }
 
     return {
-      groupsTree,
+      groups,
       onGroupClicked,
       sideBarOpen: true,
     }
@@ -175,42 +177,7 @@ export default defineComponent({
 })
 </script>
 <style>
-.groupsTree .vtl-caret {
-  margin-left: -0.9rem;
-  margin-right: 0.3rem;
-}
-.groupsTree .vtl-node-main {
-  padding: 0;
-  margin-bottom: 0.35rem;
-  margin-top: 0.35rem;
-}
-.groupsTree .vtl-node-content {
-  line-height: 20px;
-}
-.groupsTree .vtl-tree-margin {
-  margin-top: -12.5px;
-  margin-left: 1.5em;
-}
-.groupsTree .vtl-icon-caret-down:before {
-  content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" data-svgs-path="sm1/caret.svg"><path fill="none" fill-rule="evenodd" stroke="gray" stroke-linecap="round" stroke-linejoin="round" d="M16 10l-4 4-4-4"></path></svg>');
-  display: block;
-  padding-right: 0.3rem;
-  margin-left: -1.3rem;
-  margin-top: -0.1rem;
-  font-size: 0.875em;
-}
-.groupsTree .vtl-icon-caret-right:before {
-  content: url('data:image/svg+xml; utf8, <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" data-svgs-path="sm1/caret.svg"><path fill="none" fill-rule="evenodd" stroke="gray" stroke-linecap="round" stroke-linejoin="round" d="M16 10l-4 4-4-4"></path></svg>');
-  display: block;
-  margin-left: -1.5rem;
-  margin-top: -0.2rem;
-  padding-right: 0.2rem;
-  padding-left: 0.3rem;
-  font-size: 0.875em;
-  transform: rotate(-90deg);
-}
-
-.groupsTree .vtl-operation {
-  display: none;
+.groupsTree .vl-items {
+  overflow: visible;
 }
 </style>
