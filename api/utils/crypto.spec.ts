d19e426aff7e73b4c237caefe55913013722b568
Tobias Diez <code@tobiasdiez.com>
2021-11-11 13:57:52 +0100

    Fix stuff

--- api/utils/crypto.spec.ts
+++ api/utils/crypto.spec.ts
@@ -2,9 +2,13 @@ import { hash, verifyHash } from './crypto'
 
 describe('hash', () => {
   it('should return the correct hash', async () => {
-    expect(await hash('EBNPXY35TYkYXHs', 'saltsaltsaltsaltsaltsaltsaltsalt')).toBe('saltsaltsaltsaltsaltsaltsaltsalt63f7e072b6a9faf6e77616c098c4bb3ac69c58d249e620e1dd51257018ac7fcb40b576e9f69e9c556c70a980327dac12b1ee76a76f22b249d585fe2de10b365a')
+    expect(
+      await hash('EBNPXY35TYkYXHs', 'saltsaltsaltsaltsaltsaltsaltsalt')
+    ).toBe(
+      'saltsaltsaltsaltsaltsaltsaltsalt63f7e072b6a9faf6e77616c098c4bb3ac69c58d249e620e1dd51257018ac7fcb40b576e9f69e9c556c70a980327dac12b1ee76a76f22b249d585fe2de10b365a'
+    )
   })
-}
+})
 
 describe('verifyHash', () => {
   it('returns true for same password', async () => {
