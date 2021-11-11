0e634c25b68962e68c3c8efe7f926dba0ec654bd
Tobias Diez <code@tobiasdiez.com>
2021-11-11 16:37:06 +0100

    Fix user password

--- api/database/seed.ts
+++ api/database/seed.ts
@@ -13,7 +13,7 @@ async function seedInternal(prisma: PrismaClientType): Promise<void> {
       id: 'ckn4oul7100004cv7y3t94n8j',
       email: 'alice@jabref.de',
       password:
-        'saltsaltsaltsaltsaltsaltsaltsalt63f7e072b6a9faf6e77616c098c4bb3ac69c58d249e620e1dd51257018ac7fcb40b576e9f69e9c556c70a980327dac12b1ee76a76f22b249d585fe2de10b365a', // EBNPXY35TYkYXHs
+        '19184d8c1c1e9b483d8347f8da0d53ad92170233100d32c3a0d748725948c28d09a060d7f02962b7b93320c72a2cdd94f21b16b08bf8bd1cba0c5f77afeffddbb24df527c4f16f1fca6eb5480159b56df3d818b4b3c74ead04227a78b3d810b8', // EBNPXY35TYkYXHs
       name: 'Alice',
     },
   })
