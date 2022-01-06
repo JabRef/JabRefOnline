359cdb1ab2d4b11307b0ac89d3eb1e7c77067cc4
Tobias Diez <code@tobiasdiez.com>
2022-01-06 21:39:10 +0100

    Don't use redis for sessions for now

--- api/user/passport-initializer.ts
+++ api/user/passport-initializer.ts
@@ -27,15 +27,23 @@ export default class PassportInitializer {
   }
 
   install(app: Express): void {
+    // TODO: Use redis store also for development as soon as https://github.com/tj/connect-redis/issues/336 is fixed (and mock-redis is compatible with redis v4)
+    let store
+    if (config.environment === Environment.Production) {
+      const RedisStore = connectRedis(session)
+      store = new RedisStore({
+        client: this.redisClient,
+        disableTouch: true,
+      })
+    } else {
+      store = new session.MemoryStore()
+    }
+
     // Add middleware that sends and receives the session ID using cookies
     // See https://github.com/expressjs/session#readme
-    const RedisStore = connectRedis(session)
     app.use(
       session({
-        store: new RedisStore({
-          client: this.redisClient,
-          disableTouch: true,
-        }),
+        store,
         // The secret used to sign the session cookie
         secret: [config.session.primarySecret, config.session.secondarySecret],
         // Don't force session to be saved back to the session store unless it was modified
