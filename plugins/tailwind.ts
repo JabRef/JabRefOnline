307320fa6e78a387d61adef9ab90e6f3aa6016f3
Tobias Diez <code@tobiasdiez.com>
2022-04-22 16:07:45 +0000

    Convert tailwind-vue config to variantjs

--- plugins/tailwind.ts
+++ plugins/tailwind.ts
@@ -1,141 +1,147 @@
-import { variantJS, VariantJSConfiguration } from '@variantjs/vue'
-import Vue from 'vue'
 import {
-  TInput,
-  TCheckbox,
-  TButton,
-  TInputGroup,
   TAlert,
+  TButton,
+  TCheckbox,
   TDropdown,
-  TTag,
+  TInput,
+  TInputGroup,
   TSelect,
+  TTag,
   TTextarea,
-  TTable,
+  variantJS,
+  VariantJSConfiguration,
 } from '@variantjs/vue'
 
+import { defineNuxtPlugin } from '#app'
+
 const settings: VariantJSConfiguration = {
-  't-input': {
-    component: TInput,
-    props: {
-      fixedClasses:
-        'transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-      classes:
-        'block w-full text-black sm:text-sm placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500',
-      variants: {
-        error:
+  TInput: {
+    fixedClasses:
+      'transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+    classes:
+      'block w-full text-black sm:text-sm placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500',
+    variants: {
+      error: {
+        classes:
           'block w-full sm:text-sm border-error-300 bg-error-50 placeholder-error-200 text-error-900',
-        success:
+      },
+      success: {
+        classes:
           'block w-full sm:text-sm border-success-300 bg-success-50 placeholder-gray-400 text-success-900',
-        plain:
+      },
+      plain: {
+        classes:
           'shadow-none placeholder-gray-400 bg-transparent border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300',
       },
     },
   },
-  't-textarea': {
-    component: TTextarea,
-    props: {
-      fixedClasses:
-        'block w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-      classes:
-        'text-black placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500 ',
-      variants: {
-        plain:
+  TTextarea: {
+    fixedClasses:
+      'block w-full px-3 py-2 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+    classes:
+      'text-black placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500 ',
+    variants: {
+      plain: {
+        classes:
           'shadow-none placeholder-gray-400 bg-transparent border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300',
       },
     },
   },
-  't-checkbox': {
-    component: TCheckbox,
-    props: {
-      fixedClasses:
-        'transition duration-100 ease-in-out rounded shadow-sm focus:border-primary-500 focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 focus:ring-offset-0 disabled:opacity-50 disabled:cursor-not-allowed',
-      classes: 'text-primary-500 border-gray-300 ',
-      variants: {
-        error: 'text-error-500 border-error-300',
-        success: 'text-success-500 border-success-300',
+  TCheckbox: {
+    fixedClasses:
+      'transition duration-100 ease-in-out rounded shadow-sm focus:border-primary-500 focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 focus:ring-offset-0 disabled:opacity-50 disabled:cursor-not-allowed',
+    classes: 'text-primary-500 border-gray-300 ',
+    variants: {
+      error: {
+        classes: 'text-error-500 border-error-300',
+      },
+      success: {
+        classes: 'text-success-500 border-success-300',
       },
     },
   },
-  't-button': {
-    component: TButton,
-    props: {
-      fixedClasses:
-        'block transition duration-100 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none',
-      classes:
-        'px-4 py-2 focus:border-highlight focus:ring-2 focus:ring-highlight-500 focus:ring-opacity-50 text-white bg-primary-500 border border-transparent shadow-sm rounded hover:bg-highlight-600',
-      variants: {
-        secondary:
+  TButton: {
+    fixedClasses:
+      'block transition duration-100 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none',
+    classes:
+      'px-4 py-2 focus:border-highlight focus:ring-2 focus:ring-highlight-500 focus:ring-opacity-50 text-white bg-primary-500 border border-transparent shadow-sm rounded hover:bg-highlight-600',
+    variants: {
+      secondary: {
+        classes:
           'px-4 py-2 focus:border-highlight focus:ring-2 focus:ring-highlight-500 focus:ring-opacity-50 text-gray-800 bg-white border border-gray-300 shadow-sm hover:text-gray-600',
-        error:
+      },
+      error: {
+        classes:
           'px-4 py-2 focus:border-highlight-500 focus:ring-2 focus:ring-highlight-500 focus:ring-opacity-50 text-white bg-error-500 border border-transparent rounded shadow-sm hover:bg-error-600',
-        success:
+      },
+      success: {
+        classes:
           'px-4 py-2 focus:border-highlight-500 focus:ring-2 focus:ring-highlight-500 focus:ring-opacity-50 text-white bg-success-500 border border-transparent rounded shadow-sm hover:bg-green-600',
-        link: 'text-primary-500 underline hover:text-highlight-600',
-        linkplain: 'hover:text-highlight-600',
+      },
+      link: {
+        classes: 'text-primary-500 underline hover:text-highlight-600',
+      },
+      linkplain: {
+        classes: 'hover:text-highlight-600',
       },
     },
   },
-  't-input-group': {
-    component: TInputGroup,
-    props: {
-      fixedClasses: {
-        wrapper: '',
-        label: 'block text-sm text-gray-700',
-        body: 'mt-1',
-        feedback: ' text-sm text-sm mt-1',
-        description: 'text-gray-400 text-sm',
+  TInputGroup: {
+    fixedClasses: {
+      wrapper: '',
+      label: 'block text-sm text-gray-700',
+      body: 'mt-1',
+      feedback: ' text-sm text-sm mt-1',
+      description: 'text-gray-400 text-sm',
+    },
+    classes: {
+      wrapper: '',
+      label: '',
+      body: '',
+      feedback: 'text-gray-400',
+      description: 'text-gray-400',
+    },
+    variants: {
+      error: {
+        label: 'text-error-500',
+        feedback: 'text-error-500',
       },
-      classes: {
-        wrapper: '',
-        label: '',
-        body: '',
-        feedback: 'text-gray-400',
-        description: 'text-gray-400',
+      success: {
+        label: 'text-success-500',
+        feedback: 'text-success-500',
       },
-      variants: {
-        error: {
-          label: 'text-error-500',
-          feedback: 'text-error-500',
-        },
-        success: {
-          label: 'text-success-500',
-          feedback: 'text-success-500',
-        },
-        important: {
-          label: 'font-semibold',
-        },
+      important: {
+        label: 'font-semibold',
       },
     },
   },
-  't-alert': {
-    component: TAlert,
-    props: {
-      fixedClasses: {
-        wrapper: 'relative flex items-center p-4 border-l-4  rounded shadow-sm',
-        body: 'grow',
-        close:
-          'absolute relative flex items-center justify-center ml-4 shrink-0 w-6 h-6 transition duration-100 ease-in-out rounded focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50',
-        closeIcon: 'fill-current h-4 w-4',
+  TAlert: {
+    fixedClasses: {
+      wrapper: 'relative flex items-center p-4 border-l-4  rounded shadow-sm',
+      body: 'grow',
+      close:
+        'absolute relative flex items-center justify-center ml-4 shrink-0 w-6 h-6 transition duration-100 ease-in-out rounded focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50',
+      closeIcon: 'fill-current h-4 w-4',
+    },
+    classes: {
+      wrapper: 'bg-primary-50 border-primary-500',
+      body: 'text-primary-700',
+      close: 'text-primary-500 hover:bg-highlight-200',
+    },
+    variants: {
+      error: {
+        wrapper: 'bg-error-50 border-error-500',
+        body: 'text-error-700',
+        close: 'text-error-500 hover:bg-error-200',
       },
-      classes: {
-        wrapper: 'bg-primary-50 border-primary-500',
-        body: 'text-primary-700',
-        close: 'text-primary-500 hover:bg-highlight-200',
-      },
-      variants: {
-        error: {
-          wrapper: 'bg-error-50 border-error-500',
-          body: 'text-error-700',
-          close: 'text-error-500 hover:bg-error-200',
-        },
-        success: {
-          wrapper: 'bg-success-50 border-success-500',
-          body: 'text-success-700',
-          close: 'text-success-500 hover:bg-success-200',
-        },
+      success: {
+        wrapper: 'bg-success-50 border-success-500',
+        body: 'text-success-700',
+        close: 'text-success-500 hover:bg-success-200',
       },
     },
   },
+  /*
   't-nuxtlink': {
     // TButton has builtin support for NuxtLink, so we just reuse it
     component: TButton,
@@ -144,97 +150,88 @@ const settings: VariantJSConfiguration = {
       classes: 'text-primary-500 hover:text-highlight-600',
     },
   },
-  't-dropdown': {
-    component: TDropdown,
-    props: {
-      fixedClasses: {
-        button:
-          'flex items-center text-white block px-4 py-2 transition duration-100 ease-in-out border border-transparent rounded shadow-sm focus:border-highlight-500 focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-        wrapper: 'inline-flex flex-col',
-        dropdownWrapper: 'relative z-10',
-        dropdown: 'absolute rounded shadow mt-1',
-        enterActiveClass:
-          'transition ease-out duration-100 transform opacity-0 scale-95',
-        enterToClass: 'transform opacity-100 scale-100',
-        leaveClass: 'transition ease-in transform opacity-100 scale-100',
-        leaveToClass: 'transform opacity-0 scale-95 duration-75',
-      },
-      classes: {
-        button: 'bg-primary-500 hover:bg-highlight-600',
-        dropdown: 'bg-white origin-top-left left-0',
-      },
-      variants: {
-        left: {
-          dropdown: 'bg-white origin-top-right right-0',
-        },
+  */
+  TDropdown: {
+    fixedClasses: {
+      button:
+        'flex items-center text-white block px-4 py-2 transition duration-100 ease-in-out border border-transparent rounded shadow-sm focus:border-highlight-500 focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+      wrapper: 'inline-flex flex-col',
+      dropdownWrapper: 'relative z-10',
+      dropdown: 'absolute rounded shadow mt-1',
+      enterActiveClass:
+        'transition ease-out duration-100 transform opacity-0 scale-95',
+      enterToClass: 'transform opacity-100 scale-100',
+      leaveClass: 'transition ease-in transform opacity-100 scale-100',
+      leaveToClass: 'transform opacity-0 scale-95 duration-75',
+    },
+    classes: {
+      button: 'bg-primary-500 hover:bg-highlight-600',
+      dropdown: 'bg-white origin-top-left left-0',
+    },
+    variants: {
+      left: {
+        dropdown: 'bg-white origin-top-right right-0',
       },
     },
   },
-  't-tag': {
-    component: TTag,
-    props: {
-      fixedClasses: '',
-      classes: '',
-      variants: {
-        badge: 'inline-flex items-center px-2 rounded-lg',
+  TTag: {
+    fixedClasses: '',
+    classes: '',
+    variants: {
+      badge: {
+        classes: 'inline-flex items-center px-2 rounded-lg',
       },
     },
   },
-  't-select': {
-    component: TSelect,
-    props: {
-      wrapped: true,
-      fixedClasses: {
-        wrapper: 'relative inline-block text-left',
+  TSelect: {
+    wrapped: true,
+    fixedClasses: {
+      wrapper: 'relative inline-block text-left',
+      input:
+        'bg-none block w-full py-2 pl-2 pr-6 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
+      arrowWrapper:
+        'pointer-events-none absolute inset-y-0 right-0 flex items-center px-2',
+      arrow: 'fill-current h-4 w-4',
+    },
+    classes: {
+      wrapper: '',
+      input:
+        'text-black placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500 sm:text-sm',
+      arrowWrapper: 'text-gray-700',
+      arrow: '',
+    },
+    variants: {
+      plain: {
         input:
-          'bg-none block w-full py-2 pl-2 pr-6 transition duration-100 ease-in-out border rounded shadow-sm focus:ring-2 focus:ring-highlight-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed',
-        arrowWrapper:
-          'pointer-events-none absolute inset-y-0 right-0 flex items-center px-2',
-        arrow: 'fill-current h-4 w-4',
+          'shadow-none placeholder-gray-400 bg-white border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300 cursor-pointer sm:text-sm',
+        arrowWrapper: 'text-current',
       },
-      classes: {
-        wrapper: '',
+      plaincaps: {
         input:
-          'text-black placeholder-gray-400 bg-white border-gray-300 focus:border-highlight-500 sm:text-sm',
-        arrowWrapper: 'text-gray-700',
-        arrow: '',
-      },
-      variants: {
-        plain: {
-          input:
-            'shadow-none placeholder-gray-400 bg-white border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300 cursor-pointer sm:text-sm',
-          arrowWrapper: 'text-current',
-        },
-        plaincaps: {
-          input:
-            'text-current shadow-none placeholder-gray-400 bg-white border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300 cursor-pointer uppercase text-gray-600 tracking-wider focus-within:text-black sm:text-xs',
-          arrowWrapper: 'text-current',
-        },
+          'text-current shadow-none placeholder-gray-400 bg-white border-transparent focus:bg-white focus:border-highlight-500 hover:bg-gray-50 hover:border-gray-300 cursor-pointer uppercase text-gray-600 tracking-wider focus-within:text-black sm:text-xs',
+        arrowWrapper: 'text-current',
       },
     },
   },
-  't-table': {
-    component: TTable,
-    props: {
-      classes: {
-        table:
-          'min-w-full divide-y divide-gray-100 shadow-sm border-gray-200 border',
-        thead: '',
-        theadTr: '',
-        theadTh: 'px-3 py-2 font-semibold text-left bg-gray-100 border-b',
-        tbody: 'bg-white divide-y divide-gray-100',
-        tr: '',
-        td: 'px-3 py-2 whitespace-no-wrap',
-        tfoot: '',
-        tfootTr: '',
-        tfootTd: '',
-      },
-      variants: {
-        plain: {
-          table: '',
-          td: 'px-3 py-1 whitespace-no-wrap',
-          tbody: 'bg-transparent',
-        },
+  TTable: {
+    classes: {
+      table:
+        'min-w-full divide-y divide-gray-100 shadow-sm border-gray-200 border',
+      thead: '',
+      theadTr: '',
+      theadTh: 'px-3 py-2 font-semibold text-left bg-gray-100 border-b',
+      tbody: 'bg-white divide-y divide-gray-100',
+      tr: '',
+      td: 'px-3 py-2 whitespace-no-wrap',
+      tfoot: '',
+      tfootTr: '',
+      tfootTd: '',
+    },
+    variants: {
+      plain: {
+        table: '',
+        td: 'px-3 py-1 whitespace-no-wrap',
+        tbody: 'bg-transparent',
       },
     },
   },
@@ -242,4 +239,14 @@ const settings: VariantJSConfiguration = {
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(variantJS, settings)
+  nuxtApp.vueApp.component('t-input', TInput)
+  nuxtApp.vueApp.component('t-checkbox', TCheckbox)
+  nuxtApp.vueApp.component('t-button', TButton)
+  nuxtApp.vueApp.component('t-inputgroup', TInputGroup)
+  nuxtApp.vueApp.component('t-alert', TAlert)
+  nuxtApp.vueApp.component('t-dropdown', TDropdown)
+  nuxtApp.vueApp.component('t-tag', TTag)
+  nuxtApp.vueApp.component('t-select', TSelect)
+  nuxtApp.vueApp.component('t-textarea', TTextarea)
+  // nuxtApp.vueApp.component('t-table', TTable)
 })
