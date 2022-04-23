dd37022c8ced3479042db2454af45fe2da48415e
Tobias Diez <code@tobiasdiez.com>
2022-04-23 14:31:25 +0000

    Upgrade vue-jest

--- jest.config.ts
+++ jest.config.ts
@@ -10,7 +10,7 @@ const config: Config.InitialOptions = {
   moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.vue$': '@vue/vue2-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
   },
   collectCoverage: false,
   collectCoverageFrom: [
