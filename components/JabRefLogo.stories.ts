cc0da3d5d2319ccb60c496ea933878ea31165499
Tobias Diez <code@tobiasdiez.com>
2022-05-11 23:32:45 +0200

    Add story for logo (#1058)

---
+++ components/JabRefLogo.stories.ts
@@ -0,0 +1,11 @@
+import JabRefLogo from './JabrefLogo.vue'
+
+export default {
+  title: 'JabRef Logo',
+  component: JabRefLogo,
+}
+
+export const Default = () => ({
+  components: { JabRefLogo },
+  template: '<JabRefLogo />',
+})
