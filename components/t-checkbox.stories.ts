04e0d0c1759b2880019ed80b45d7549880c7f9b5
Mohit <62339705+mohit038@users.noreply.github.com>
2022-05-21 01:44:03 +0530

    Add storybook stories for remaining tailwind components (#1081)
    * add stories
    
    * remove

---
+++ components/t-checkbox.stories.ts
@@ -0,0 +1,40 @@
+import { Story, Meta } from '@storybook/vue'
+import { TCheckbox } from 'vue-tailwind/dist/components'
+
+export default {
+  component: TCheckbox,
+  title: 't-checkbox',
+  args: {
+    checked: true,
+  },
+} as Meta
+
+const Template: Story = (args) => ({
+  setup() {
+    return { args }
+  },
+  template: '<t-checkbox v-bind="args" />',
+})
+
+export const Default = Template.bind({})
+Default.args = {
+  checked: true,
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  variant: 'error',
+  checked: true,
+}
+
+export const Success = Template.bind({})
+Success.args = {
+  variant: 'success',
+  checked: true,
+}
+
+export const Plain = Template.bind({})
+Plain.args = {
+  variant: 'plain',
+  checked: true,
+}
