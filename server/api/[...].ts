5986fda664d68cee9774f592b269e7ef6d6c282c
Tobias Diez <code@tobiasdiez.com>
2022-06-19 21:37:50 +0000

    Add event handler

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -22,31 +22,35 @@ if (useRuntimeConfig().public.environment === Environment.Production) {
 const httpServer = http.createServer(app)
 
 // TODO: Replace this with await, once esbuild supports top-level await
-void configureTsyringe().then(() => {
-  const passportInitializer = resolve('PassportInitializer')
-  passportInitializer.initialize()
-  passportInitializer.install(app)
+void configureTsyringe()
+  .then(() => {
+    const passportInitializer = resolve('PassportInitializer')
+    passportInitializer.initialize()
+    passportInitializer.install(app)
 
-  const server = new ApolloServer({
-    schema: loadSchema(),
-    context: buildContext,
-    introspection: true,
-    plugins: [
-      // Enable Apollo Studio in development, and also in production (at least for now)
-      ApolloServerPluginLandingPageLocalDefault({ footer: false }),
-      // Gracefully shutdown HTTP server when Apollo server terminates
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-    ],
-    // Only reply to requests with a Content-Type header to prevent CSRF and XS-Search attacks
-    // https://www.apollographql.com/docs/apollo-server/security/cors/#preventing-cross-site-request-forgery-csrf
-    csrfPrevention: true,
+    const server = new ApolloServer({
+      schema: loadSchema(),
+      context: buildContext,
+      introspection: true,
+      plugins: [
+        // Enable Apollo Studio in development, and also in production (at least for now)
+        ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+        // Gracefully shutdown HTTP server when Apollo server terminates
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+      ],
+      // Only reply to requests with a Content-Type header to prevent CSRF and XS-Search attacks
+      // https://www.apollographql.com/docs/apollo-server/security/cors/#preventing-cross-site-request-forgery-csrf
+      csrfPrevention: true,
+    })
+
+    async function startServer() {
+      await server.start()
+      server.applyMiddleware({ app, path: '/' })
+    }
+    void startServer()
   })
-
-  async function startServer() {
-    await server.start()
-    server.applyMiddleware({ app, path: '/' })
-  }
-  void startServer()
-})
+  .catch((error) => {
+    console.error('Error while executing configureTsyringe', error)
+  })
 
 export default app
