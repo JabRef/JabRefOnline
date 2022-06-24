8f65c5c876d2b96a30b46202e0e8d75f95314f7d
Tobias Diez <tobiasdiez@gmx.de>
2022-06-24 12:24:35 +0200

    Add styleing to table

--- components/TTable.vue
+++ components/TTable.vue
@@ -1,25 +1,16 @@
 <!-- Simle table, needed as long as variantjs doesn't provide one https://github.com/variantjs/vue/issues/26 -->
 <template>
-  <table>
+  <table :class="tableClass">
     <thead>
       <tr>
-        <th
-          v-for="header in headers"
-          :key="header"
-        >
+        <th v-for="header in headers" :key="header" :class="headerClass">
           {{ header }}
         </th>
       </tr>
     </thead>
-    <tbody>
-      <tr
-        v-for="(row, index) in data"
-        :key="index"
-      >
-        <td
-          v-for="cell in row"
-          :key="cell"
-        >
+    <tbody :class="tbodyClass">
+      <tr v-for="(row, index) in data" :key="index">
+        <td v-for="cell in row" :key="cell" :class="tdClass">
           {{ cell }}
         </td>
       </tr>
@@ -38,4 +29,15 @@ const props = withDefaults(defineProps<Props>(), {
   data: () => [],
   variant: '',
 })
+
+let tableClass =
+  'min-w-full divide-y divide-gray-100 shadow-sm border-gray-200 border'
+let headerClass = 'px-3 py-2 font-semibold text-left bg-gray-100 border-b'
+let tbodyClass = 'bg-white divide-y divide-gray-100'
+let tdClass = 'px-3 py-2 whitespace-no-wrap'
+if (props.variant == 'plain') {
+  tableClass = ''
+  tbodyClass = 'bg-transparent'
+  tdClass = 'px-3 py-1 whitespace-no-wrap'
+}
 </script>
