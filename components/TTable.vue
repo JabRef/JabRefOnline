f8eb8a01065a2dd668de80241e5d918d5ec9c661
Tobias Diez <tobiasdiez@gmx.de>
2022-06-24 12:48:41 +0200

    Fix style

--- components/TTable.vue
+++ components/TTable.vue
@@ -3,14 +3,25 @@
   <table :class="tableClass">
     <thead>
       <tr>
-        <th v-for="header in headers" :key="header" :class="headerClass">
+        <th
+          v-for="header in headers"
+          :key="header"
+          :class="headerClass"
+          >
           {{ header }}
         </th>
       </tr>
     </thead>
     <tbody :class="tbodyClass">
-      <tr v-for="(row, index) in data" :key="index">
-        <td v-for="cell in row" :key="cell" :class="tdClass">
+      <tr
+        v-for="(row, index) in data"
+        :key="index"
+        >
+        <td
+          v-for="cell in row"
+          :key="cell"
+          :class="tdClass"
+          >
           {{ cell }}
         </td>
       </tr>
@@ -32,10 +43,10 @@ const props = withDefaults(defineProps<Props>(), {
 
 let tableClass =
   'min-w-full divide-y divide-gray-100 shadow-sm border-gray-200 border'
-let headerClass = 'px-3 py-2 font-semibold text-left bg-gray-100 border-b'
+const headerClass = 'px-3 py-2 font-semibold text-left bg-gray-100 border-b'
 let tbodyClass = 'bg-white divide-y divide-gray-100'
 let tdClass = 'px-3 py-2 whitespace-no-wrap'
-if (props.variant == 'plain') {
+if (props.variant === 'plain') {
   tableClass = ''
   tbodyClass = 'bg-transparent'
   tdClass = 'px-3 py-1 whitespace-no-wrap'
