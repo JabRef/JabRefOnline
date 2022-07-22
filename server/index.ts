d73726724df01e790da707347cd87ceb99db4fe4
Tobias Diez <code@tobiasdiez.com>
2022-07-22 19:42:34 +0200

    Only allow cors from apollo-studio

--- server/index.ts
+++ server/index.ts
@@ -117,8 +117,9 @@ export default defineLazyEventHandler(async () => {
   return server.createHandler({
     path: '/api',
     cors: {
-      // Allow requests from all domains
-      origin: '*',
+      // Allow requests Apollo Studio: https://www.apollographql.com/docs/studio/explorer/connecting-authenticating/
+      origin: "https://studio.apollographql.com",
+      credentials: true
     }
   })
 })
