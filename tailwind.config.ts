ec6731bd20a7b2faa2518bfc37429e7ea02ff877
Tobias Diez <code@tobiasdiez.com>
2022-08-03 13:29:01 +0000

    Migrate tailwind config to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,29 @@
-// Need to be a CommonJS module for now
-/* eslint-disable */
+import { Config } from 'tailwindcss'
+
 // TODO: Renable lint as soon as this file is converted to typescript
-const colors = require('tailwindcss/colors')
-const plugin = require('tailwindcss/plugin')
+import colors from 'tailwindcss/colors'
+import plugin from 'tailwindcss/plugin'
+
+// @ts-ignore: has no type info
+import lineClamp from '@tailwindcss/line-clamp'
+import forms from '@tailwindcss/forms'
 
 // Flip object horizontally or vertically
 // Adapted from https://github.com/tailwindlabs/tailwindcss/discussions/2146
-const flip = plugin(({ addUtilities, theme, variants }) => {
-  const newUtilities = {
+const flip = plugin(({ addUtilities }) => {
+  addUtilities({
     '.flip-horizontal': {
       '--tw-scale-x': '-1',
     },
     '.flip-vertical': {
       '--tw-scale-y': '-1',
     },
-  }
-  addUtilities(newUtilities, variants('flip'))
+  })
 })
 
-module.exports = {
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/forms'),
-    flip,
-  ],
+export default <Config>{
+  content: [`components/**/*.{vue,js}`, `layouts/**/*.vue`, `pages/**/*.vue`],
+  plugins: [lineClamp, forms, flip],
   theme: {
     colors: {
       transparent: 'transparent',
