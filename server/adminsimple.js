41a26a018f600a7d8172ec31d86d0f1cbbfc83e8
Tobias Diez <code@tobiasdiez.com>
2022-08-30 11:36:19 +0000

    Add standalone admin server

---
+++ server/adminsimple.js
@@ -0,0 +1,17 @@
+/* export default defineEventHandler((event) => {
+    console.log(event.req.url)
+  }) */
+
+import { createAgent } from '@forestadmin/agent'
+import { createSqlDataSource } from '@forestadmin/datasource-sql'
+
+// Create your Forest Admin agent
+const agent = createAgent({
+  // These process.env variables should be provided in the onboarding
+  authSecret: 'd6746eed59164eff30872857ff703043a65da3f4a56986d6', // process.env.FOREST_AUTH_SECRET,
+  agentUrl: 'http://localhost:3000/_admin/', // process.env.FOREST_AGENT_URL,
+  envSecret: 'a4e47c2af50f0ca42d01b5d5bea6bccddf2f4a1b3f7a3ee56ba17d3c556aabfe', // process.env.FOREST_ENV_SECRET,
+  isProduction: false,
+  prefix: '_admin',
+}).addDataSource(createSqlDataSource(process.env.DATABASE_URL))
+agent.mountOnStandaloneServer(3000).start()
