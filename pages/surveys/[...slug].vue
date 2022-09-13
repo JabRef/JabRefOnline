4583ce44f33327a1ffc2588fc077dc170678af30
Tobias Diez <code@tobiasdiez.com>
2022-09-13 17:54:48 +0000

    Remove workaround for index page of surveys

--- pages/surveys/[...slug].vue
+++ pages/surveys/[...slug].vue
@@ -16,11 +16,8 @@ let markdownPath = 'surveys/'
 if (typeof route.params.slug === 'string') {
   markdownPath += route.params.slug
 } else {
-  // Workaround for https://github.com/nuxt/content/issues/1237: use /home.md instead /index.md as default
-  const slugs = route.params.slug
-  if (slugs[slugs.length - 1] === '') {
-    slugs[slugs.length - 1] = 'home'
-  }
-  markdownPath += slugs.map((param) => param.toLowerCase()).join('/')
+  markdownPath += route.params.slug
+    .map((param) => param.toLowerCase())
+    .join('/')
 }
 </script>
