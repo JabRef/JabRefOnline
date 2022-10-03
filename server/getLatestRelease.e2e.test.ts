3219a5a4c7e2be5ea818ece34da92200b294194c
Tobias Diez <code@tobiasdiez.com>
2022-10-03 11:13:07 +0000

    fix tests

--- server/getLatestRelease.e2e.test.ts
+++ server/getLatestRelease.e2e.test.ts
@@ -1,9 +1,14 @@
 import { root } from '~/test/api-e2e/supertest'
 
+type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T
+type GetLatestReleaseResponse = Awaited<
+  ReturnType<typeof import('~/server/api/getLatestRelease').default>
+>
+
 it('Returns a valid version', async () => {
   const response = await root().get('/api/getLatestRelease')
 
   expect(response.statusCode).toBe(200)
   expect(response.get('content-type')).toContain('application/json')
-  expect(response.text).toMatch(/^{\n {2}"version": "\d.\d"\n}$/)
+  expect((response.body as GetLatestReleaseResponse).version).toMatch(/^\d.\d$/)
 })
