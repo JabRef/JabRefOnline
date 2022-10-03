6e708adb7cb2da7edb5f0ba4b8dbf896b4a22550
Tobias Diez <code@tobiasdiez.com>
2022-10-03 12:27:51 +0000

    simplify github call

--- server/api/getLatestRelease.ts
+++ server/api/getLatestRelease.ts
@@ -1,42 +1,34 @@
-import { ApolloClient, gql, HttpLink, InMemoryCache } from '@apollo/client/core'
-
 const config = useRuntimeConfig()
-const github = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'https://api.github.com/graphql',
-    headers: {
-      Authorization: `Bearer ${config.githubRepoToken}`,
-    },
-  }),
-  defaultOptions: {
-    query: {
-      fetchPolicy: 'no-cache',
-    },
-  },
-})
 
 export default defineEventHandler(async () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { data } = await github.query({
-    query: gql`
-      query LatestRelease {
-        # eslint-disable-next-line @graphql-eslint/fields-on-correct-type
-        repository(owner: "JabRef", name: "jabref") {
-          releases(first: 1, orderBy: { field: CREATED_AT, direction: DESC }) {
-            nodes {
-              tagName
+  const response = await fetch('https://api.github.com/graphql', {
+    headers: { Authorization: `Bearer ${config.githubRepoToken}` },
+    body: JSON.stringify({
+      query: `
+        query LatestRelease {
+          # eslint-disable-next-line @graphql-eslint/fields-on-correct-type
+          repository(owner: "JabRef", name: "jabref") {
+            releases(
+              first: 1
+              orderBy: { field: CREATED_AT, direction: DESC }
+            ) {
+              nodes {
+                tagName
+              }
             }
           }
         }
-      }
-    `,
-  })
+      `,
+    }),
+    method: 'POST',
+  }).then((res) => res.json())
   return {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    version: (data.repository.releases.nodes[0].tagName as string).replace(
-      'v',
-      ''
-    ), // something like 5.7
+    version:
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      (response.data.repository.releases.nodes[0].tagName as string).replace(
+        'v',
+        ''
+      ), // something like 5.7
   }
 })
