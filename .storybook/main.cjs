d7f1580f29b98d85e20aef8e24590519f72ec5cd
Tobias Diez <code@tobiasdiez.de>
2023-01-24 15:06:09 +0800

    specify stories

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,9 +1,11 @@
 module.exports = {
+  // Need to specify stories as workaround for https://github.com/storybookjs/storybook/issues/20761
+  stories: ['../components/*.stories.ts'],
   core: {
-    disableTelemetry: true
+    disableTelemetry: true,
   },
   framework: {
-    name: "@storybook/vue3-vite",
-    options: {}
-  }
-};
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+}
