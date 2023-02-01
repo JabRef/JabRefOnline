ffa87cc6cebf79308efb82ea505079ee83a78a10
Tobias Diez <code@tobiasdiez.de>
2023-02-01 22:03:35 +0800

    chore: migrate to native vue storybook stories

---
+++ components/JabRefLogo.stories.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import JabRefLogo from './JabrefLogo.vue'
+</script>
+
+<template>
+  <Stories
+    title="JabRef Logo"
+    :component="JabRefLogo"
+  >
+    <Story title="Default">
+      <JabRefLogo />
+    </Story>
+  </Stories>
+</template>
---
+++ components/n-button.stories.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+</script>
+<template>
+  <Stories
+    title="n-button"
+    :component="NButton"
+  >
+    <Story title="Default">
+      <n-button />
+    </Story>
+    <Story title="Error">
+      <n-button type="error"> error </n-button>
+    </Story>
+    <Story title="Success">
+      <n-button type="success"> success </n-button>
+    </Story>
+    <Story title="Primary">
+      <n-button type="primary"> primary </n-button>
+    </Story>
+    <Story title="Tertiary">
+      <n-button type="tertiary"> tertiary </n-button>
+    </Story>
+    <Story title="Link">
+      <n-button text>Link</n-button>
+    </Story>
+  </Stories>
+</template>
