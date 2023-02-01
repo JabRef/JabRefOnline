45fc34fdaca4af9d232d91e4d98a729be7d8fbac
Tobias Diez <code@tobiasdiez.com>
2023-02-01 22:29:25 +0800

    chore: migrate to native vue storybook stories (#1729)
    Co-authored-by: Tobias Diez <code@tobiasdiez.de>

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,8 +1,10 @@
 module.exports = {
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|vue)'],
   core: {
     // Disable telemetry collection
     disableTelemetry: true,
     // Use vite as builder
     builder: '@storybook/builder-vite',
   },
+  addons: ['storybook-vue-addon'],
 }
