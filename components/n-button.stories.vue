d13ffb5a7a8886995f98917ec3c367b95bbf7647
Tobias Diez <code@tobiasdiez.de>
2023-02-01 22:13:41 +0800

    readd default button name

--- components/n-button.stories.vue
+++ components/n-button.stories.vue
@@ -7,19 +7,19 @@ import { NButton } from 'naive-ui'
     :component="NButton"
   >
     <Story title="Default">
-      <n-button />
+      <n-button>Button</n-button>
     </Story>
     <Story title="Error">
-      <n-button type="error"> error </n-button>
+      <n-button type="error">error</n-button>
     </Story>
     <Story title="Success">
-      <n-button type="success"> success </n-button>
+      <n-button type="success">success</n-button>
     </Story>
     <Story title="Primary">
-      <n-button type="primary"> primary </n-button>
+      <n-button type="primary">primary</n-button>
     </Story>
     <Story title="Tertiary">
-      <n-button type="tertiary"> tertiary </n-button>
+      <n-button type="tertiary">tertiary</n-button>
     </Story>
     <Story title="Link">
       <n-button text>Link</n-button>
