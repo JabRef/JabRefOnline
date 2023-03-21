78b6a1fd9abb621c57b59d599eadd2ec7705c929
Tobias Diez <code@tobiasdiez.com>
2023-03-21 15:19:43 +0100

    chore: add devtools (#1893)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,6 +75,9 @@ export default defineNuxtConfig({
     // Add support for native vue stories
     // https://github.com/tobiasdiez/storybook-vue-addon
     'storybook-vue-addon/nuxt',
+    // Devtools support
+    // https://github.com/nuxt/devtools
+    '@nuxt/devtools',
   ],
 
   /*
