5f60f872def44b5b20a92cb5d0e62fd25dae86b5
Tobias Diez <code@tobiasdiez.de>
2023-09-22 22:15:35 +0800

    add redis test

--- server/utils/services.factory.ts
+++ server/utils/services.factory.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */ // TODO: Remove once redis-mock is updated
 import { Config, Environment } from '~/config'
 
+import { Redis } from 'ioredis'
 import { createStorage, Storage } from 'unstorage'
 import memoryDriver from 'unstorage/drivers/memory'
 
@@ -32,6 +33,7 @@ export async function createRedisClient(config: Config): Promise<Storage> {
             },
           }
         case Environment.CI:
+        default:
           // Redis on Github Actions does not need a password
           return {
             port: config.redis.port,
@@ -39,6 +41,22 @@ export async function createRedisClient(config: Config): Promise<Storage> {
           }
       }
     }
+
+    // Create redis instance to test connection
+    const redisClient = new Redis(createRedisConfig())
+    redisClient.on('error', (error) => {
+      console.error('Redis error', error)
+    })
+    redisClient.on('ready', () => {
+      console.warn('Redis ready')
+    })
+    redisClient.on('connect', () => {
+      console.warn('Redis connected')
+    })
+    redisClient.on('close', () => {
+      console.warn('Redis closed')
+    })
+    await redisClient.set('test', 'test')
 
     const { default: redisDriver } = await import('unstorage/drivers/redis')
     return createStorage({
