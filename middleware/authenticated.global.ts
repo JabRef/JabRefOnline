cfff2b83b17e541e631645e7f1762f575519c290
Tobias Diez <code@tobiasdiez.de>
2024-04-16 10:01:28 +0800

    fix ts

--- middleware/authenticated.global.ts
+++ middleware/authenticated.global.ts
@@ -4,7 +4,11 @@ import { gql } from '@apollo/client/core'
  * Plugin that adds checks if the user is logged in, and redirects her to the login page if not.
  */
 export default defineNuxtRouteMiddleware(async (to, _from) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
+  if (
+    to.matched.some(
+      (record: { meta: { requiresAuth: any } }) => record.meta.requiresAuth,
+    )
+  ) {
     const { $apolloClient } = useNuxtApp()
     try {
       // TODO: Only call this if we have a session cookie?
