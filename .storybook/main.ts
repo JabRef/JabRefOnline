f5102d8be59924d567bd7aaf0e753e689349ba75
Tobias Diez <code@tobiasdiez.de>
2024-08-11 20:48:59 +0200

    disable vue stories for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,8 @@ import type { StorybookConfig } from '@storybook-vue/nuxt'
 
 const config: StorybookConfig = {
   // Need to specify stories as workaround for https://github.com/storybookjs/storybook/issues/20761
-  stories: ['../components/*.stories.@(vue|ts)'],
+  //stories: ['../components/*.stories.@(vue|ts)'],
+  stories: ['../components/*.stories.@(ts)'],
   core: {
     disableTelemetry: true,
   },
