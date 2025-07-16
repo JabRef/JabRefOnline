640eecac0e58d6dfb9ac5c3994b48943b02aa0c6
Tobias Diez <code@tobiasdiez.de>
2025-07-16 17:48:40 +0200

    fix prettier

--- apollo/cache.ts
+++ apollo/cache.ts
@@ -1,4 +1,4 @@
-import { type ApolloCache, InMemoryCache  } from '@apollo/client/core'
+import { type ApolloCache, InMemoryCache } from '@apollo/client/core'
 import { relayStylePagination } from '@apollo/client/utilities/policies/pagination'
 import introspection from '~/apollo/introspection'
 import { gql } from '.'
