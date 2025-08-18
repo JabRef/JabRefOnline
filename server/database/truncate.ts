f1fe3b4277eb5f5eead2ebe6854d7e310e0914ee
Tobias Diez <code@tobiasdiez.de>
2025-08-18 19:59:24 +0800

    Fix error: Failed to deserialize column of type 'name'

--- server/database/truncate.ts
+++ server/database/truncate.ts
@@ -18,14 +18,14 @@ export async function truncate(): Promise<void> {
 
   try {
     for (const { tablename } of (await prisma.$queryRawUnsafe(
-      `SELECT tablename FROM pg_tables WHERE schemaname='${dbSchemaName}';`,
+      `SELECT tablename::text FROM pg_tables WHERE schemaname='${dbSchemaName}';`,
     )) as any) {
       await prisma.$queryRawUnsafe(
         `TRUNCATE TABLE "${dbSchemaName}"."${tablename as string}" CASCADE;`,
       )
     }
     for (const { relname } of (await prisma.$queryRawUnsafe(
-      `SELECT c.relname FROM pg_class AS c JOIN pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind='S' AND n.nspname='${dbSchemaName}';`,
+      `SELECT c.relname::text FROM pg_class AS c JOIN pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind='S' AND n.nspname='${dbSchemaName}';`,
     )) as any) {
       await prisma.$queryRawUnsafe(
         `ALTER SEQUENCE "${dbSchemaName}"."${
