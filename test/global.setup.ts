8542b3723ee9f913a718c730057451b3875be936
Tobias Diez <code@tobiasdiez.de>
2025-09-07 16:38:39 +0800

    fix integration test

--- test/global.setup.ts
+++ test/global.setup.ts
@@ -30,6 +30,12 @@ beforeAll((context) => {
 
   if (isIntegrationTest) {
     const config = constructConfig()
+    // Override config from environment variables (only for db connections)
+    config.databaseUrl = process.env.NUXT_DATABASE_URL ?? config.databaseUrl
+    config.redis.port = process.env.NUXT_REDIS_PORT ?? config.redis.port
+    config.redis.host = process.env.NUXT_REDIS_HOST ?? config.redis.host
+    config.redis.password =
+      process.env.NUXT_REDIS_PASSWORD ?? config.redis.password
     register('Config', {
       useValue: config,
     })
