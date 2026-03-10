c16a6579a1aa1e4886a9f5107209d1c3cb28c4ca
Tobias Diez <code@tobiasdiez.de>
2026-03-10 14:20:09 +0100

    fix path to migrations

--- prisma.config.ts
+++ prisma.config.ts
@@ -16,7 +16,7 @@ const shadowDatabaseUrl = process.env.NUXT_SHADOW_DATABASE_URL
 export default defineConfig({
   schema: path.join('server'),
   migrations: {
-    path: path.join('server', 'database', 'migrations'),
+    path: path.join('server', 'migrations'),
     seed: 'jiti ./server/database/runSeed.ts',
   },
   ...(databaseUrl !== undefined
